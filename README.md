5d7f4c2c13921cff26e9e27e695f8709a39a262c
Yoann Fleury <yoann.fleury@smartpanda.com>
2017-04-04 14:41:57 +0200

    feat: init repository

---
+++ README.md
@@ -0,0 +1,7 @@
+# Highlight Cat
+
+Small ZSH script to highlight source code using `cat` and `highlight`.
+
+## Author
+
+Yoann Fleury
---
+++ hc.zsh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+alias hc='highlight_cat'
+
+highlight_cat() {
+  if [ ! -x "$(which highlight)" ]; then
+    echo "Package \'highlight\' is not installed!"
+    return -1
+  fi
+
+  if [ $# -eq 0 ]; then
+    cat $@ | highlight --out-format=xterm256 --syntax=js
+  fi
+
+  for FNAME in $@
+  do
+    filename=$(basename "$FNAME")
+    extension="${filename##*.}"
+    cat $FNAME | highlight --out-format=xterm256 --syntax=$extension
+  done
+}
